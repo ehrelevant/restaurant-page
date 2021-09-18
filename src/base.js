dfbe17f087817383ea9a28c391f954e8e4b9cf5f
KindaExists <ehrencastillo@gmail.com>
2021-09-18 13:55:51 +0800

    add base template loader

--- src/base.js
+++ src/base.js
@@ -2,15 +2,38 @@ import {homepageQueue} from './homepage.js';
 
 const content = document.querySelector('#content')
 
-function displayPage(pageQueue, content) {
-    content.textContent = '';
-    for (const element of homepageQueue) {
-        content.appendChild(element);
+// Loads and displays base content container
+// Everything loaded here won't be reloaded, so they don't need to be saved in a queue
+function loadBase(container){
+    const pickHomeBtn = document.createElement('button');
+    pickHomeBtn.textContent = 'Home';
+    const pickMenuBtn = document.createElement('button');
+    pickMenuBtn.textContent = 'Menu';
+    const pickContactBtn = document.createElement('button');
+    pickContactBtn.textContent = 'Contact';
+
+    const pageContent = document.createElement('div');
+
+    appendNewContent([pickHomeBtn, pickMenuBtn, pickContactBtn, pageContent], container)
+    return pageContent;
+}
+
+
+// Appends children/page content based on the queue order (pageQueue)
+function appendNewContent(pageQueue, container) {
+    // Clears page content
+    container.textContent = '';
+
+    // Appends new page content
+    for (const element of pageQueue) {
+        container.appendChild(element);
     }
 }
 
+
 function initialize(){
-    displayPage(homepageQueue, content);
+    const baseContent = loadBase(content);
+    appendNewContent(homepageQueue, baseContent);
 }
 
 export {initialize};
